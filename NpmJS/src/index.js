9d16c35d1e725db4e62dd11e386600fab83d5dee
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2024-08-11 13:23:49 -0600

    progress bar working

--- NpmJS/src/index.js
+++ NpmJS/src/index.js
@@ -1,5 +1,21 @@
-import { scroll } from 'motion';
+import { animate, scroll } from "motion";
 
-scroll((info) => {
-  console.log(info);
-});
+window.progressAnimation = function () {
+
+  const experiencesCtn = document.getElementById("experiences-ctn");
+  const experiences = experiencesCtn.querySelectorAll("div");
+
+  document.querySelector('#progress-bar').style.transformOrigin = 'top';
+
+  const scrollOptions = {target: experiencesCtn, offset: ["start end", "end end"]}
+
+  scroll(animate('#progress-bar', { scaleY: [0, 1] }));
+
+  experiences.forEach(experienceCtn => {
+    scroll(animate(experienceCtn, { y: [-50, 50] }), {
+      target: experienceCtn
+    });
+
+  });
+
+};
