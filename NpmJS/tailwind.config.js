ad5e4067bd6bda7b6c8ff91676705fa9fa6b717b
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2025-01-21 16:07:11 -0700

    responsive pt1

--- NpmJS/tailwind.config.js
+++ NpmJS/tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: ["../Pages/**/*.{razor,html,cshtml}", "../Layout/**/*.{razor,html,cshtml}", "../wwwroot/index.html"],
   theme: {
     extend: {
+      textColor: ['group-hover', 'hover'],
+      stroke: ['group-hover', 'hover'],
       fontFamily: {
         aleo: ['Aleo-Regular', 'sans-serif'],
         aleoBold: ['Aleo-Bold', 'sans-serif'],
