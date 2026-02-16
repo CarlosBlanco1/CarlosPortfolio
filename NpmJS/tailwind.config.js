732e7ffd9694963949e3753754731347eceb9ed8
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2026-02-16 14:40:33 -0500

    added some responsiveness

--- NpmJS/tailwind.config.js
+++ NpmJS/tailwind.config.js
@@ -1,16 +1,27 @@
 module.exports = {
-  content: ["../Pages/**/*.{razor,html,cshtml}", "../Layout/**/*.{razor,html,cshtml}", "../wwwroot/index.html"],
+  content: [
+    "../Pages/**/*.{razor,html,cshtml}",
+    "../Layout/**/*.{razor,html,cshtml}",
+    "../wwwroot/index.html",
+  ],
   theme: {
     extend: {
-      textColor: ['group-hover', 'hover'],
-      stroke: ['group-hover', 'hover'],
+      textColor: ["group-hover", "hover"],
+      stroke: ["group-hover", "hover"],
       fontFamily: {
-        aleo: ['Aleo-Regular', 'sans-serif'],
-        aleoBold: ['Aleo-Bold', 'sans-serif'],
-        neuropolitical: ['Neuropolitical', 'sans-serif'],
-        proggy: ['Proggy', 'sans-serif'],
+        aleo: ["Aleo-Regular", "sans-serif"],
+        aleoBold: ["Aleo-Bold", "sans-serif"],
+        neuropolitical: ["Neuropolitical", "sans-serif"],
+        proggy: ["Proggy", "sans-serif"],
       },
+    },
+    screens: {
+      sm: "733px",
+      md: "845px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
   },
   plugins: [],
-}
+};
